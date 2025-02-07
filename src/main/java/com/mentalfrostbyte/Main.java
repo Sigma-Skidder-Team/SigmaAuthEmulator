a581104f588292f656a6c2fafccaf57b567c91e8
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-02-07 21:04:24 +0200

    captcha generating

--- src/main/java/com/mentalfrostbyte/Main.java
+++ src/main/java/com/mentalfrostbyte/Main.java
@@ -2,12 +2,19 @@ package com.mentalfrostbyte;
 
 import com.mentalfrostbyte.emulator.SigmaHandler;
 import com.mentalfrostbyte.utils.SSLUtil;
+import com.mewebstudio.captcha.Captcha;
+import com.mewebstudio.captcha.Config;
+import com.mewebstudio.captcha.GeneratedCaptcha;
 import com.sun.net.httpserver.*;
 
 import javax.net.ssl.*;
+import java.awt.*;
 import java.net.InetSocketAddress;
 
 public class Main {
+
+    public static Captcha captcha;
+    public static GeneratedCaptcha generatedCaptcha;
 
     public static void main(String[] args) throws Exception {
         System.out.println(
@@ -19,6 +26,14 @@ public class Main {
                         "|_______/    |__|  \\______| |__|  |__| /__/     \\__\\ \n"
         );
         System.out.println("Starting Sigma Emulator V3 made by lexi");
+
+        final Config config = new Config();
+        config.setWidth(70);
+        config.setHeight(70);
+        captcha = new Captcha(config);
+        generatedCaptcha = captcha.generate();
+
+        System.out.println("Generated a new captcha with the code: " + generatedCaptcha.getCode());
 
         SSLContext sslContext = SSLUtil.createSSLContext("keystore.p12", "123456");
         HttpsServer server = HttpsServer.create(new InetSocketAddress(443), 0);
